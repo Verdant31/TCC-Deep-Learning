6102d22ea8c219c38d6ba3a90a6e601f944d71b6
verdant31 <joao.pedro3110@hotmail.com>
2024-04-07 18:39:17 -0300

    using only test data to show metrics

--- scraping.py
+++ scraping.py
@@ -4,9 +4,17 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
 import time
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--allow-running-insecure-content')
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+chrome_options.add_argument(f'user-agent={user_agent}')
+
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://conteudos.xpi.com.br/acoes/bbas3/grafico/")
 
 chart = driver.find_element(By.ID, "grafico-atv")
@@ -23,6 +31,4 @@ time.sleep(3)
 el = driver.find_element(By.CSS_SELECTOR, '[data-value="stepline"]')
 el.click()  
 
-
-
-time.sleep(9999)
+driver.save_screenshot("test.png")
